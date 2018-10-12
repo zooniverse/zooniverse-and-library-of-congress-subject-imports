e2e2b6875f4e9319dc45f6446f36191144aa96ba
mcbouslog <mcbouslog@gmail.com>
2018-10-12 15:00:43 -0500

    Simplify variable

--- import_subjects.py
+++ import_subjects.py
@@ -3,7 +3,7 @@ import json
 import os
 from panoptes_client import Panoptes, Project, SubjectSet, Subject
 
-LIBRARY_OF_CONGRESS_ITEM_ID = os.getenv('LOC_ITEM_ID') # item ID, set via environment variable. Use 'mss5186201' for demonstration purposes, see README
+LOC_ITEM_ID = os.getenv('LOC_ITEM_ID') # item ID, set via environment variable. Use 'mss5186201' for demonstration purposes, see README
 
 USERNAME = os.getenv('PANOPTES_USERNAME') # your Zooniverse username, set via environment variable
 PASSWORD = os.getenv('PANOPTES_PASSWORD') # your Zooniverse password, set via environment variable
@@ -51,7 +51,7 @@ def transform_item_segments(url, segments = []):
     print('Item segments transformation complete.')
     return segments
 
-segments = transform_item_segments('https://www.loc.gov/item/' + LIBRARY_OF_CONGRESS_ITEM_ID)
+segments = transform_item_segments('https://www.loc.gov/item/' + LOC_ITEM_ID)
 
 Panoptes.connect(username=USERNAME, password=PASSWORD, endpoint=ENDPOINT)
 
